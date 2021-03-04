bb55499645f6b7f3bde9ae2fdc23063d760dc442
sebasvalencia726 <sebasvalencia726@gmail.com>
2021-03-04 12:24:56 -0500

    make it better again

--- app/views.py
+++ app/views.py
@@ -9,34 +9,34 @@ from django.template import loader
 from django.http import HttpResponse
 from django import template
 
-@login_required(login_url="/login/")
+#@login_required(login_url="/login/")
 def index(request):
-    
+
     context = {}
     context['segment'] = 'index'
 
     html_template = loader.get_template( 'index.html' )
     return HttpResponse(html_template.render(context, request))
 
-@login_required(login_url="/login/")
+#@login_required(login_url="/login/")
 def pages(request):
     context = {}
     # All resource paths end in .html.
     # Pick out the html file name from the url. And load that template.
     try:
-        
+
         load_template      = request.path.split('/')[-1]
         context['segment'] = load_template
-        
+
         html_template = loader.get_template( load_template )
         return HttpResponse(html_template.render(context, request))
-        
+
     except template.TemplateDoesNotExist:
 
         html_template = loader.get_template( 'page-404.html' )
         return HttpResponse(html_template.render(context, request))
 
     except:
-    
+
         html_template = loader.get_template( 'page-500.html' )
         return HttpResponse(html_template.render(context, request))
