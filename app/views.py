e270e13668e39c48738ed5b4223082fe1f1e782d
sebasvalencia726 <sebasvalencia726@gmail.com>
2021-03-07 16:04:33 -0500

    make it better again

--- app/views.py
+++ app/views.py
@@ -8,11 +8,73 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.template import loader
 from django.http import HttpResponse
 from django import template
+from re import X
 import pandas as pd
-bar_chart = __import__('/home/sebastian/Holberton/majorityreportmedellin/app/plotly_functions.py').bar_chart
-line_chart = __import__('/home/sebastian/Holberton/majorityreportmedellin/app/plotly_functions.py').line_chart
+import plotly.express as px
+from plotly.offline import plot
 
-#@login_required(login_url="/login/")
+def bar_chart(df, x_data, y_data, color=None):
+    """Funtion to return plotly object and displays a bar chart
+
+    Args:
+        df (data frame): dataframe to be used
+        x_data (str): column to display as x axis
+        y_data (str): column to display as y axis
+        color (str, optional): Bar colors, defaults to None.
+
+    Returns:
+        Object: plot_div  object
+    """
+    if color is not None:
+        pivot_data = df.pivot_table(index=[x_data, color],
+                                    values=y_data,
+                                    aggfunc='sum').reset_index()
+    else:
+        pivot_data = df.pivot_table(index=x_data,
+                                    values=y_data,
+                                    aggfunc='sum').reset_index()
+
+    fig = px.bar(pivot_data,
+                x=x_data,
+                y=y_data,
+                color=color,
+                template="plotly_white",
+                color_discrete_sequence=px.colors.qualitative.Pastel)
+
+    plot_div = plot(fig, output_type='div')
+    return plot_div
+
+
+def line_chart(df, x_data, color, y_data):
+    """Funtion to return plotly object and displays a line chart
+
+    Args:
+        df (data frame): dataframe to be used
+        x_data (str): column to display as x axis
+        color (str): y axis to display.
+        y_data (str): column to display as y axis
+
+
+    Returns:
+        Object: plot_div  object
+    """
+    pivot_data = df.pivot_table(index=[x_data, color],
+                                values=y_data,
+                                aggfunc='sum')
+
+    pivot_data.index.set_names([x_data, color], inplace=True)
+    graph = pivot_data.reset_index()
+    fig = px.line(graph,
+                x=color,
+                y=y_data,
+                color=x_data,
+                template="plotly_white",
+                color_discrete_sequence=px.colors.qualitative.Pastel)
+
+    plot_div = plot(fig, output_type='div')
+    return plot_div
+
+
 def index(request):
 
     context = {'title': 'Majority Report Medellín',
