87bf681b783b481e916898571378b8a1a7b5e4a0
Sebastian Valencia <sebastian.valencia@pragma.com.co>
2021-06-21 08:45:55 -0500

    update

--- core/settings.py
+++ core/settings.py
@@ -124,15 +124,5 @@ STATICFILES_DIRS = (
 )
 #############################################################
 #############################################################
-"""AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = 'majoritybucket'
-
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-
-STATIC_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/'
-ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
-"""
 #############################################################
 #############################################################
