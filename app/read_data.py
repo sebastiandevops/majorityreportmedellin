30fae1f6dca41c321d46c938bb818691d3dd7df8
Sebastian Valencia <sebastian.valencia@pragma.com.co>
2021-06-23 11:00:37 -0500

    updatedatabase

---
+++ app/read_data.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""function to read and filter data"""
+if __name__ == "__main__":
+    import pandas as pd
+    import sys
+    data = pd.read_csv(sys.argv[1]).query("año >= 2018")
+    data.to_csv("data_filtered.csv")
